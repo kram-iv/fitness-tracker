c06e85bc061ea895f9520f2b99d34d012aab7e99
kram-iv <vikram.karanth@gmail.com>
2018-02-22 08:29:51 +1100

    first commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule,
+            MatIconModule,
+            MatFormFieldModule,
+            MatInputModule,
+            MatDatepickerModule,
+            MatNativeDateModule],
+  exports: [MatButtonModule,
+            MatIconModule,
+            MatFormFieldModule,
+            MatInputModule,
+            MatDatepickerModule,
+            MatNativeDateModule]
+})
+export class MaterialModule {}
